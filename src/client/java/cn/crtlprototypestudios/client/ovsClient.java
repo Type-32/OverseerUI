2ace66a8408706a08b5f5f4d2e7b628397757f3d
Type-32 <87076491+Type-32@users.noreply.github.com>
2024-07-15 16:04:37 +0800

    Init Commit

---
+++ src/client/java/cn/crtlprototypestudios/client/ovsClient.java
@@ -0,0 +1,10 @@
+package cn.crtlprototypestudios.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ovsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/cn/crtlprototypestudios/ovs.java
@@ -0,0 +1,10 @@
+package cn.crtlprototypestudios;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ovs implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
