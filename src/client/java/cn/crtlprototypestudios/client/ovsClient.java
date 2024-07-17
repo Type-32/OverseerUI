bd4b0e478e8021e5b7c043268f77bf64306c038a
Type-32 <87076491+Type-32@users.noreply.github.com>
2024-07-17 23:37:31 +0800

    Init Commit

--- src/client/java/cn/crtlprototypestudios/client/ovsClient.java
+++ src/client/java/cn/crtlprototypestudios/client/ovsClient.java
@@ -1,10 +1,29 @@
 package cn.crtlprototypestudios.client;
 
+import cn.crtlprototypestudios.client.gui.utility.ScreenStackUtils;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import org.lwjgl.glfw.GLFW;
 
+@Environment(EnvType.CLIENT)
 public class ovsClient implements ClientModInitializer {
 
+    private static final KeyBinding TOGGLEUI = new KeyBinding("key.control_ui.toggle", GLFW.GLFW_KEY_B, "key.category.control_ui");
     @Override
     public void onInitializeClient() {
+        KeyBindingHelper.registerKeyBinding(TOGGLEUI);
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (TOGGLEUI.wasPressed()){
+                ScreenStackUtils.to(new MainMenuScreen());
+            }
+            if (client.currentScreen == null){
+                ScreenStackUtils.clear();
+            }
+        });
+        System.out.println("Control UI Mod Client-Side Initialized!");
     }
 }
