557def8127e9b3317ef65a525eaacbdf33c3b339
Type-32 <87076491+Type-32@users.noreply.github.com>
2025-02-03 18:16:27 +0800

    chore(foundation): layouts and adding children to base component

--- src/main/java/cn/crtlprototypestudios/ovsr/api/components/BaseComponent.java
+++ src/main/java/cn/crtlprototypestudios/ovsr/api/components/BaseComponent.java
@@ -3,10 +3,15 @@ package cn.crtlprototypestudios.ovsr.api.components;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class BaseComponent {
     protected int x, y, width, height;
     protected boolean visible = true;
     protected Component tooltip;
+    protected List<BaseComponent> children = new ArrayList<>();
+    protected BaseComponent parent;
 
     public BaseComponent(int x, int y, int width, int height) {
         this.x = x;
@@ -38,4 +43,55 @@ public abstract class BaseComponent {
     public void setVisible(boolean visible) { this.visible = visible; }
     public Component getTooltip() { return tooltip; }
     public void setTooltip(Component tooltip) { this.tooltip = tooltip; }
+
+    public void setWidth(int cellWidth) {
+        this.width = cellWidth;
+    }
+
+    public void setHeight(int cellHeight) {
+        this.height = cellHeight;
+    }
+
+    public void addChild(BaseComponent child) {
+        children.add(child);
+        child.setParent(this);
+        onChildAdded(child);
+    }
+
+    public void removeChild(BaseComponent child) {
+        children.remove(child);
+        child.setParent(null);
+        onChildRemoved(child);
+    }
+
+    public void clearChildren() {
+        for (BaseComponent child : new ArrayList<>(children)) {
+            removeChild(child);
+        }
+    }
+
+    public List<BaseComponent> getChildren() {
+        return children;
+    }
+
+    protected void setParent(BaseComponent parent) {
+        this.parent = parent;
+    }
+
+    public BaseComponent getParent() {
+        return parent;
+    }
+
+    // Optional hooks for layouts to respond to child changes
+    protected void onChildAdded(BaseComponent child) {}
+    protected void onChildRemoved(BaseComponent child) {}
+
+    // Get absolute position (considering parent positions)
+    public int getAbsoluteX() {
+        return (parent != null ? parent.getAbsoluteX() : 0) + x;
+    }
+
+    public int getAbsoluteY() {
+        return (parent != null ? parent.getAbsoluteY() : 0) + y;
+    }
 }
