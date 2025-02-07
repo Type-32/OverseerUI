98ca8be26e32df6871009be6a12616e914e8832f
Type-32 <87076491+Type-32@users.noreply.github.com>
2025-02-07 11:12:14 +0800

    exp(declarative-ui): ok/

--- src/main/java/cn/crtlprototypestudios/ovsr/client/mixin/GlStateManagerMixin.java
+++ src/main/java/cn/crtlprototypestudios/ovsr/client/mixin/GlStateManagerMixin.java
@@ -8,25 +8,30 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import org.joml.Vector2d;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @OnlyIn(Dist.CLIENT)
 @Mixin(value = GlStateManager.class, remap = false)
 public class GlStateManagerMixin {
 
-    @WrapMethod(method = "_viewport")
-    private static void viewport(int i, int j, int k, int l, Operation<Void> original) {
-        if (!ViewportScaling.isChanged()) {
-            original.call(i, j, k, l);
-            return;
+    @Inject(method = "_viewport", at = @At("HEAD"), cancellable = true)
+    private static void onViewport(int i, int j, int k, int l, CallbackInfo ci) {
+        if (ViewportScaling.isChanged()) {
+            GlStateManager._viewport(ViewportScaling.X_OFFSET,
+                    ViewportScaling.Y_TOP_OFFSET,
+                    ViewportScaling.WIDTH,
+                    ViewportScaling.HEIGHT);
+            ci.cancel();
         }
-        original.call(ViewportScaling.X_OFFSET, ViewportScaling.Y_TOP_OFFSET, ViewportScaling.WIDTH, ViewportScaling.HEIGHT);
     }
 
-    @WrapMethod(method = "_scissorBox")
-    private static void scissorBox(int i, int j, int k, int l, Operation<Void> original) {
+    @Inject(method = "_scissorBox", at = @At("HEAD"), cancellable = true)
+    private static void onScissorBox(int i, int j, int k, int l, CallbackInfo ci) {
         Vector2d xy = ViewportScaling.scalePoint(i, j);
         Vector2d wh = ViewportScaling.scaleWidthHeight(k, l);
-        original.call((int) xy.x, (int) xy.y, (int) wh.x, (int) wh.y);
+        GlStateManager._scissorBox((int) xy.x, (int) xy.y, (int) wh.x, (int) wh.y);
+        ci.cancel();
     }
-
 }
--- src/main/java/cn/crtlprototypestudios/ovsr/client/mixin/MouseHandlerMixin.java
+++ src/main/java/cn/crtlprototypestudios/ovsr/client/mixin/MouseHandlerMixin.java
@@ -13,30 +13,38 @@ import org.joml.Vector2d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @OnlyIn(Dist.CLIENT)
 @Mixin(MouseHandler.class)
 public class MouseHandlerMixin {
-//    @Inject(method = "onPress", at = @At("HEAD"), cancellable = true)
-//    private void onMouseButton(long window, int button, int action, int mods, CallbackInfo ci) {
-//        if (ImGui.getIO().getWantCaptureMouse()) {
-//            ci.cancel();
-//        }
-//    }
-    @WrapMethod(method = "onMove")
-    public void onMove(long l, double d, double e, Operation<Void> original) {
-        Vector2d scaled = ViewportScaling.unscalePoint(d, e);
-        original.call(l, scaled.x, scaled.y);
+    @ModifyVariable(method = "onMove",
+            at = @At("HEAD"),
+            ordinal = 0, argsOnly = true)
+    private double modifyX(double x) {
+        return ViewportScaling.unscalePoint(x, 0).x;
     }
 
-    @WrapOperation(method = {"grabMouse", "releaseMouse"}, at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/platform/Window;getScreenWidth()I"))
-    public int calculateDoubledCentreX(Window instance, Operation<Integer> original) {
+    @ModifyVariable(method = "onMove",
+            at = @At("HEAD"),
+            ordinal = 1, argsOnly = true)
+    private double modifyY(double y) {
+        return ViewportScaling.unscalePoint(0, y).y;
+    }
+
+    @Redirect(method = {"grabMouse", "releaseMouse"},
+            at = @At(value = "INVOKE",
+                    target = "Lcom/mojang/blaze3d/platform/Window;getScreenWidth()I"))
+    public int calculateDoubledCentreX(Window instance) {
         return (ViewportScaling.X_OFFSET + (ViewportScaling.WIDTH/2)) * 2;
     }
 
-    @WrapOperation(method = {"grabMouse", "releaseMouse"}, at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/platform/Window;getScreenHeight()I"))
-    public int calculateDoubledCentreY(Window instance, Operation<Integer> original) {
+    @Redirect(method = {"grabMouse", "releaseMouse"},
+            at = @At(value = "INVOKE",
+                    target = "Lcom/mojang/blaze3d/platform/Window;getScreenHeight()I"))
+    public int calculateDoubledCentreY(Window instance) {
         return (ViewportScaling.Y_OFFSET + (ViewportScaling.HEIGHT/2)) * 2;
     }
 }
